b8e44a29dbe1897aea8252037967ad9650bf21af
Laleh Mohtashami <laleh.mohtashami@gmail.com>
2019-10-17 17:51:37 +0330

    ajax request: get data

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,31 @@
-import React from 'react';
-import Head from 'next/head';
-import Nav from '../comps/nav';
 import Layout from '../comps/Layout';
-import './index.css';
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
-const Home = () => (
-	<div>
-		<Head>
-			<title>Home</title>
-			<link rel="icon" href="/favicon.ico" />
-		</Head>
-    <Layout>
-      
-    </Layout>
-		
-	</div>
+const Index = props => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {/* {props.shows.map(show => (
+        <li key={show.id}>
+          <Link href="/p/[id]" as={`/p/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))} */}
+    </ul>
+  </Layout>
 );
 
-export default Home;
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  // const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  // return {
+  //   shows: data.map(entry => entry.show)
+  // };
+};
+
+export default Index;

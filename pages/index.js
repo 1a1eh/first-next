be3e06e0392b0888fa824a2c7fa99670fe7a3f9d
Laleh Mohtashami <laleh.mohtashami@gmail.com>
2019-10-23 17:39:01 +0330

    index chenges

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
+import { useRouter } from 'next/router';
+import Layout from '../components/Layout.js';
 
-import Layout from '../components/Layout';
-
-export default function Index() {
+const Content = () => {
+  const router = useRouter();
   return (
-    <Layout>
-      <p>Hello Next.js</p>
-    </Layout>
+    <>
+      <h1>{router.query.title}</h1>
+      <p>This is the blog post content.</p>
+    </>
   );
-}
+};
+
+const Page = () => (
+  <Layout>
+    <Content />
+  </Layout>
+);
+
+export default Page;

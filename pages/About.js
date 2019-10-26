316548345e950ce92f217d4a4cc5bed2dd3da435
Laleh Mohtashami <laleh.mohtashami@gmail.com>
2019-10-26 10:40:03 +0330

    trying fetch

--- pages/About.js
+++ pages/About.js
@@ -1,10 +1,31 @@
-
 import Layout from '../components/Layout';
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
-export default function About() {
-  return (
-    <Layout>
-      <p>This is the about page</p>
-    </Layout>
-  );
-}
+const About = props => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link href="/p/[id]" as={`/p/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+);
+
+About.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import { useRouter } from 'next/router';
 import Layout from '../components/Layout.js';
+import './style.css';
 
 const Content = () => {
   const router = useRouter();
   return (
     <>
       <h1>{router.query.title}</h1>
-      <p>This is the blog post content.</p>
+      <p>This is the blog post contensdasdawsdet.</p>
     </>
   );
 };

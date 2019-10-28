a653d94f1dee0f31859ccb17b8ced4474d0918db
Laleh Mohtashami <laleh.mohtashami@gmail.com>
2019-10-28 17:23:38 +0330

    post

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,19 +1,19 @@
 import Layout from '../../components/MyLayout';
 import fetch from 'isomorphic-unfetch';
+import Link from 'next/link';
 
-const Post = props => (
-  <Layout>
-    <h1>{props.show.title}</h1>
-    <button onClick={}>view</button>
-  </Layout>
+const Post = (props) => (
+	<Layout>
+		<h1>{props.show.title}</h1>
+	</Layout>
 );
 
 Post.getInitialProps = async function(context) {
-  const { id } = context.query;
-  const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
-  const show = await res.json();
+	const { id } = context.query;
+	const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+	const show = await res.json();
 
-  return { show };
+	return { show };
 };
 
 export default Post;

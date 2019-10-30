47fc2eb12de9a721df165843f033efd6ed005527
Laleh Mohtashami <laleh.mohtashami@gmail.com>
2019-10-30 12:42:41 +0330

    fetch data

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -3,22 +3,23 @@ import fetch from 'isomorphic-unfetch';
 import '../style.css';
 
 class Post extends React.Component {
+	static async getInitialProps(context) {
+		const { id } = context.query;
+		const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+		const show = await res.json();
+
+		return { show };
+	};
+	
 	render() {
+		const { show } = this.props;
 		return (
 			<Layout>
-				<i className="green">ID: (( {this.props.show.id} ))</i>
-				<p>{this.props.show.title}</p>
+				<i className="green">ID: (( {show.id} ))</i>
+				<p>{show.title}</p>
 			</Layout>
 		);
-	}
-}
-
-Post.getInitialProps = async function(context) {
-	const { id } = context.query;
-	const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
-	const show = await res.json();
-
-	return { show };
+	};
 };
 
 export default Post;
--- pages/todos.js
+++ pages/todos.js
@@ -1,23 +1,23 @@
 import Layout from '../../components/MyLayout';
-// import fetch from 'isomorphic-unfetch';
-import axios from 'axios';
-
-const Todos = props => (
-  <Layout>
-    <h1>{props.show.name}</h1>
-    <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
-    <img src={props.show.image.medium} />
-  </Layout>
-);
-
-Todos.getInitialProps = async function(context) {
-  const { id } = context.query;
-  const res = await axios.post(`https://jsonplaceholder.typicode.com/todos/${id}`);
-  // const show = await res.json();
-
-  console.log(res);
-
-  // return { show };
+import fetch from 'isomorphic-unfetch';
+class Todos extends React.Component {
+	static async getInitialProps(context) {
+		const { id } = context.query;
+		const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+		const show = await res.json();
+		return { show };
+  };
+  
+	render() {
+		const { show } = this.props;
+		return (
+			<Layout>
+				<h1>{show.name}</h1>
+				<p>{show.summary.replace(/<[/]?[pb]>/g, '')}</p>
+				<img src={show.image.medium} />
+			</Layout>
+		);
+	}
 };
 
 export default Todos;
